983be13317b9eccfdd2c0427ba365f662cd6775b
Byron <byronjones77@gmail.com>
2023-03-19 18:48:02 -0400

    add commands and readme

--- main.py
+++ main.py
@@ -13,6 +13,18 @@ client = discord.Client(intents=discord.Intents.all())
 model_engine = "text-davinci-002"
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
+# Define news API key
+news_api_key = os.getenv("NEWSAPI_KEY")
+
+# Define Alpha Vantage API Key
+ALPHA_VANTAGE_API_KEY = os.getenv("ALPHA_VANTAGE_API_KEY")
+
+# Define Music Match API key
+MUSICMATCH_API_KEY = os.getenv("MUSIC_MATCH_API_KEY")
+
+# Define Spoonacular API key
+SPOONACULAR_API_KEY = os.getenv("SPOONACULAR_API_KEY")
+
 # Define the prefix for the bot commands
 prefix = "!"
 
@@ -21,16 +33,28 @@ USER_RESPONSE_TIME = 60.0
 
 # Define the commands and their descriptions
 commands = {
-    "question": "Ask the bot a question and get a response.",
+    "%'command'": "Command to receive a response privately.",
+    "catfact": "Get a random cat fact.",
+    "commands": "Display a list of available commands.",
     "define": "Get a definition from Urban Dictionary",
+    "joke": "Get a random joke.",
+    "movie": "Get a random movie recommendation.",
+    "news": "Get recent news articles.",
+    "question": "Ask the bot a question and get a response.",
     "reddit": "Get a specified number of the most recent posts(max 10) from a given subreddit.",
     "roll": "Roll a random number between 1 and 6.",
-    "weather": "Get the current weather for a location.",
-    "movie": "Get a random movie recommendation.",
-    "catfact": "Get a random cat fact.",
-    "joke": "Get a random joke.",
-    "commands": "Display a list of available commands.",
-    "%'command'": "Command to receive a response privately."
+    "song": "Get a song title based on lyrics.",
+    "stock": "Get current stock data.",
+    "recipe": "Get recipe for a food.",
+    "trivia": "Get a random trivia question",
+    "weather": "Get the current weather for a location."
+}
+
+# Define the Open Trivia DB API endpoint and default parameters
+trivia_api_url = 'https://opentdb.com/api.php'
+trivia_params = {
+    'amount': 1,  # Get only one question
+    'type': 'multiple',  # Get only multiple choice questions
 }
 
 
@@ -88,9 +112,9 @@ async def on_message(message):
                     await message.author.send(response.choices[0].text.strip())
             except requests.exceptions.HTTPError as e:
                 if not is_private:
-                    await message.channel.send(f"An error occurred: {e}")
+                    await message.channel.send(f"{e}")
                 else:
-                    await message.author.send(f"An error occurred: {e}")
+                    await message.author.send(f"{e}")
 
         elif command == "roll":
             # Roll a random number between 1 and 6
@@ -370,6 +394,271 @@ async def on_message(message):
                 else:
                     await message.author.send(f"An error occurred: {e}")
 
+        if command == "trivia":
+            # Fetch the trivia data
+            response = requests.get("https://opentdb.com/api.php?amount=1&type=multiple")
+            data = response.json()["results"][0]
+
+            # Send the question to the user
+            question = f"**Category:** {data['category']}\n\n{data['question']}\n\n"
+
+            # Add the answer choices
+            choices = data['incorrect_answers'] + [data['correct_answer']]
+            random.shuffle(choices)
+            for i, choice in enumerate(choices):
+                question += f"{i + 1}. {choice}\n"
+
+            if not is_private:
+                await message.channel.send(question + "\nPlease enter the number of the correct answer.")
+            else:
+                await message.author.send(question + "\nPlease enter the number of the correct answer.")
+
+            # Wait for the user to respond with an answer
+            try:
+                answer_message = await client.wait_for(
+                    "message",
+                    timeout=USER_RESPONSE_TIME,
+                    check=lambda
+                        m: m.author == message.author and m.channel == message.channel and not m.content.startswith(
+                        prefix)
+                )
+                user_answer = int(answer_message.content) - 1
+                correct_answer = choices.index(data['correct_answer'])
+                if user_answer == correct_answer:
+                    if not is_private:
+                        await message.channel.send("Correct! :white_check_mark: \nYou answered correctly.")
+                    else:
+                        await message.author.send("Correct! :white_check_mark: \nYou answered correctly.")
+                else:
+                    if not is_private:
+                        await message.channel.send(
+                            f"Incorrect! :x: \nThe correct answer was **{data['correct_answer']}**.")
+                    else:
+                        await message.author.send(
+                            f"Incorrect! :x: \nThe correct answer was **{data['correct_answer']}**.")
+            except asyncio.TimeoutError:
+                if not is_private:
+                    await message.channel.send("Sorry, you took too long to answer.")
+                else:
+                    await message.author.send("Sorry, you took too long to answer.")
+
+        elif command == "news":
+            news_api_url = "https://newsapi.org/v2/top-headlines"
+            valid_categories = ['business', 'entertainment', 'general', 'health', 'science', 'sports', 'technology']
+
+            # Get the user's preferred news category
+            if not is_private:
+                await message.channel.send("Please enter a news category \n(e.g., 'business', 'entertainment', "
+                                           "'general', 'health', 'science', 'sports', 'technology'):")
+            else:
+                await message.author.send("Please enter a news category \n(e.g., 'business', 'entertainment', "
+                                          "'general', 'health', 'science', 'sports', 'technology'):")
+            try:
+                category_message = await client.wait_for(
+                    "message",
+                    timeout=USER_RESPONSE_TIME,
+                    check=lambda m: m.author == message.author and not m.content.startswith(prefix)
+                )
+            except asyncio.TimeoutError:
+                if not is_private:
+                    await message.channel.send("Sorry, you took too long to enter a news category!")
+                else:
+                    await message.author.send("Sorry, you took too long to enter a news category!")
+                return
+
+            category = category_message.content.lower()
+
+            if category not in valid_categories:
+                if not is_private:
+                    await message.channel.send(f"Sorry, {category} is not a valid news category. Please try your "
+                                               f"command again")
+                else:
+                    await message.author.send(f"Sorry, {category} is not a valid news category. Please try your "
+                                              f"command again")
+                return
+
+            # Get the news articles for the specified category
+            try:
+                response = requests.get(news_api_url,
+                                        params={"category": category, "apiKey": news_api_key, "language": "en"})
+                response.raise_for_status()
+                articles = response.json()["articles"]
+            except (requests.exceptions.HTTPError, KeyError) as e:
+                if not is_private:
+                    await message.channel.send(f"An error occurred: {e}")
+                else:
+                    await message.author.send(f"An error occurred: {e}")
+                return
+
+            # Send the news articles to the user
+            for article in articles:
+                article_message = f"{article['title']}\n{article['url']}"
+                if not is_private:
+                    await message.channel.send(article_message)
+                else:
+                    await message.author.send(article_message)
+
+        elif command == "stock":
+            # Ask the user for the stock symbol
+            if not is_private:
+                await message.channel.send("Please enter a stock symbol (e.g., AAPL, GOOG):")
+            else:
+                await message.author.send("Please enter a stock symbol (e.g., AAPL, GOOG):")
+
+            # Wait for the user to respond with the stock symbol
+            try:
+                stock_message = await client.wait_for(
+                    "message",
+                    timeout=USER_RESPONSE_TIME,
+                    check=lambda m: m.author == message.author and not m.content.startswith(prefix)
+                )
+            except asyncio.TimeoutError:
+                if not is_private:
+                    await message.channel.send("Sorry, you took too long to enter a stock symbol!")
+                else:
+                    await message.author.send("Sorry, you took too long to enter a stock symbol!")
+                return
+
+            # Use the Alpha Vantage API to get stock data for the specified symbol
+            stock_symbol = stock_message.content.upper()
+            api_url = f"https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol={stock_symbol}&apikey={ALPHA_VANTAGE_API_KEY}"
+            try:
+                response = requests.get(api_url)
+                response.raise_for_status()
+                data = response.json()["Global Quote"]
+            except (requests.exceptions.HTTPError, KeyError) as e:
+                if not is_private:
+                    await message.channel.send(f"An error occurred: {e}")
+                else:
+                    await message.author.send(f"An error occurred: {e}")
+                return
+
+            # Check if all fields are N/A
+            if all(value == "N/A" for value in data.values()):
+                if not is_private:
+                    await message.channel.send(f"Stock data not found for symbol: {stock_symbol}")
+                else:
+                    await message.author.send(f"Stock data not found for symbol: {stock_symbol}")
+                return
+
+            # Format the stock data and send it to the user
+            stock_data = f"Symbol: {data['01. symbol']}\nPrice: {data['05. price']}\nChange: {data['09. change']}\nPercent Change: {data['10. change percent']}"
+            if not is_private:
+                await message.channel.send(stock_data)
+            else:
+                await message.author.send(stock_data)
+
+        elif command == "song":
+            if not is_private:
+                await message.channel.send("Please enter the lyrics:")
+            else:
+                await message.author.send("Please enter the lyrics:")
+
+            # Wait for the user to respond with the lyrics
+            try:
+                lyrics_message = await client.wait_for(
+                    "message",
+                    timeout=USER_RESPONSE_TIME,
+                    check=lambda m: m.author == message.author and not m.content.startswith(prefix)
+                )
+            except asyncio.TimeoutError:
+                if not is_private:
+                    await message.channel.send("Sorry, you took too long to enter the lyrics!")
+                else:
+                    await message.author.send("Sorry, you took too long to enter the lyrics!")
+                return
+
+            # Use the Musicmatch API to search for a song with the given lyrics
+            lyrics = lyrics_message.content
+            api_url = f"http://api.musixmatch.com/ws/1.1/track.search?q_lyrics={lyrics}&apikey={MUSICMATCH_API_KEY}"
+            try:
+                response = requests.get(api_url)
+                response.raise_for_status()
+                data = response.json()
+                track_list = data["message"]["body"]["track_list"]
+                if not track_list:
+                    raise ValueError("No song found with the given lyrics.")
+                track_id = track_list[0]["track"]["track_id"]
+                track_name = track_list[0]["track"]["track_name"]
+                artist_name = track_list[0]["track"]["artist_name"]
+            except (requests.exceptions.HTTPError, KeyError, ValueError) as e:
+                if not is_private:
+                    await message.channel.send(f"An error occurred: {e}")
+                else:
+                    await message.author.send(f"An error occurred: {e}")
+                return
+
+            # Send the song information to the user
+            song_data = f"Song: {track_name}\nArtist: {artist_name}"
+            if not is_private:
+                await message.channel.send(song_data)
+            else:
+                await message.author.send(song_data)
+
+        elif command == "recipe":
+            # Ask the user for the recipe they want to search for
+            if not is_private:
+                await message.channel.send("What recipe do you want to search for?")
+            else:
+                await message.author.send("What recipe do you want to search for?")
+
+            # Wait for the user to respond with the recipe they want to search for
+            try:
+                recipe_message = await client.wait_for(
+                    "message",
+                    timeout=USER_RESPONSE_TIME,
+                    check=lambda
+                        m: m.author == message.author and m.channel == message.channel and not m.content.startswith(
+                        prefix)
+                )
+            except asyncio.TimeoutError:
+                if not is_private:
+                    await message.channel.send("Sorry, you took too long to enter a recipe!")
+                else:
+                    await message.author.send("Sorry, you took too long to enter a recipe!")
+                return
+
+            # Search for recipes using the Spoonacular API
+            url = f"https://api.spoonacular.com/recipes/complexSearch?query={recipe_message.content}&apiKey={os.getenv('SPOONACULAR_API_KEY')}"
+            try:
+                response = requests.get(url).json()
+
+                # Check if any recipes were found
+                if response["results"]:
+                    # Get the first recipe from the search results
+                    recipe = response["results"][0]
+                    recipe_name = recipe["title"]
+                    recipe_id = recipe["id"]
+
+                    # Get information about the recipe using the recipe ID
+                    url = f"https://api.spoonacular.com/recipes/{recipe_id}/information?apiKey={os.getenv('SPOONACULAR_API_KEY')}"
+                    response = requests.get(url).json()
+
+                    # Get the recipe instructions and ingredients
+                    recipe_instructions = response["instructions"]
+                    recipe_ingredients = response["extendedIngredients"]
+
+                    # Format the recipe ingredients
+                    formatted_ingredients = "\n".join(
+                        [f"{ingredient['original']} ({ingredient['amount']} {ingredient['unit']})" for ingredient in
+                         recipe_ingredients])
+
+                    # Send the recipe information to the user
+                    recipe_text = f"**{recipe_name}**\n\nIngredients:\n{formatted_ingredients}\n\nInstructions:\n{recipe_instructions}"
+                    if not is_private:
+                        await message.channel.send(recipe_text)
+                    else:
+                        await message.author.send(recipe_text)
+                else:
+                    if not is_private:
+                        await message.channel.send("No recipes found!")
+                    else:
+                        await message.author.send("No recipes found!")
+            except requests.exceptions.HTTPError as e:
+                if not is_private:
+                    await message.channel.send(f"{e}")
+                else:
+                    await message.author.send(f"{e}")
 
         elif command == "commands":
             # Construct the list of commands and their descriptions
---
+++ readme.md
@@ -0,0 +1,78 @@
+<h1>Discord Bot</h1>
+This is a Python-based Discord chatbot that is designed to perform a number of tasks, such as providing trivia questions, getting stock data, and recommending movies or recipes.
+
+
+## Features
+Returns a variety of information based on user requests
+<br>Uses OpenAI GPT-3 to answer user questions
+Provides weather data for specified locations
+Returns random jokes and cat facts
+Recommends movies and recipes based on user input
+Can retrieve current stock data and recent news articles
+
+## Prerequisites
+Python 3.8+
+Discord account
+API keys for OpenAI, NewsAPI, Alpha Vantage, Music Match, and Spoonacular
+
+
+## Installation
+Clone the repository to your local machine
+Install the required dependencies using pip:
+<br>
+```pip install -r requirements.txt```
+<br>Create a .env file in the same directory as the bot.py file.
+Add your API keys to the .env file:
+```
+OPENAI_API_KEY = <your OpenAI API key>
+NEWSAPI_KEY = <your NewsAPI key>
+ALPHA_VANTAGE_API_KEY = <your Alpha Vantage API key>
+MUSIC_MATCH_API_KEY = <your Music Match API key>
+SPOONACULAR_API_KEY = <your Spoonacular API key>
+```
+To run the bot, you will need to install the following Python packages:
+<ul>
+<li>discord
+<li>requests
+<li>dotenv
+<li>openai
+</ul>
+
+Run the bot using:
+<br>```python bot.py```
+## Usage
+To use the bot, start by typing !commands to see a list of available commands.
+<br>To request information using the bot, type !<command> followed by the required parameters.
+<br>Some commands may require additional input from the user, in which case the bot will prompt the user to enter the required information.
+<br>If a command is preceded with %, the bot will respond privately to the user.
+
+The bot supports the following commands:
+<ul>
+<li>%command: Command to receive a response privately.
+<li>catfact: Get a random cat fact.
+<li>commands: Display a list of available commands.
+<li>define: Get a definition from Urban Dictionary
+<li>joke: Get a random joke.
+<li>movie: Get a random movie recommendation.
+<li>news: Get recent news articles.
+<li>question: Ask the bot a question and get a response.
+<li>reddit: Get a specified number of the most recent posts (max 10) from a given subreddit.
+<li>roll: Roll a random number between 1 and 6.
+<li>song: Get a song title based on lyrics.
+<li>stock: Get current stock data.
+<li>recipe: Get recipe for a food.
+<li>trivia: Get a random trivia question
+<li>weather: Get the current weather for a location.
+</ul>
+
+## Credits
+This bot was created by Byron Jones. It uses the following APIs:
+<ul>
+<li>OpenAI
+<li>News API
+<li>Alpha Vantage
+<li>Music Match
+<li>Spoonacular
+<li>Cat Fact API
+<li>Open Trivia DB API
+</ul>
