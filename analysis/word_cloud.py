bf485f59e586b6eb867fe5d0a4ffb69ce4d9a104
Byron <byronjones77@gmail.com>
2023-10-08 14:42:41 -0400

    updated word cloud to create asynchronously

--- analysis/word_cloud.py
+++ analysis/word_cloud.py
@@ -1,26 +1,19 @@
+import asyncio
+from concurrent.futures import ThreadPoolExecutor
 import keys
 import pandas as pd
 from wordcloud import WordCloud
 import matplotlib.pyplot as plt
 from database.db import connect_to_db
 
+executor = ThreadPoolExecutor()
 
-def create_word_cloud(servername):
-    cnxn, cursor = connect_to_db()
 
-    # SQL query to fetch data
-    query = f"""
-    SELECT message
-    FROM {keys.DISCORD_LOGS_TABLE_NAME}
-    WHERE servername='{servername}'
-    """
+async def create_word_cloud(servername):
+    loop = asyncio.get_event_loop()
 
-    cursor.execute(query)
-    rows = cursor.fetchall()
-    data = [row[0] for row in rows]
-
-    # Convert the data to a DataFrame
-    df = pd.DataFrame(data, columns=['message'])
+    # Run the database operations in a separate thread
+    df = await loop.run_in_executor(executor, fetch_data, servername)
 
     # Check if data is retrieved successfully
     if df.empty:
@@ -34,5 +27,27 @@ def create_word_cloud(servername):
     plt.axis('off')
     plt.show()
 
+
+def fetch_data(servername):
+    cnxn, cursor = connect_to_db()
+
+    query = f"""
+    SELECT message
+    FROM {keys.DISCORD_LOGS_TABLE_NAME}
+    WHERE servername='{servername}'
+    """
+
+    cursor.execute(query)
+    rows = cursor.fetchall()
+    data = [row[0] for row in rows]
+
     cursor.close()
     cnxn.close()
+
+    # Convert the data to a DataFrame and return
+    return pd.DataFrame(data, columns=['message'])
+
+
+# Run the asynchronous function
+loop = asyncio.get_event_loop()
+loop.run_until_complete(create_word_cloud("TestServer"))
