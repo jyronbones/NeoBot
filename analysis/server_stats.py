d027d713605d87f42e105f6c813d102552aa89ae
Byron <byronjones77@gmail.com>
2023-10-11 00:41:51 -0400

    added additional server stats

--- analysis/server_stats.py
+++ analysis/server_stats.py
@@ -1,13 +1,60 @@
-from database.fetch_users import fetch_top_users
+from discord import File
+
+from analysis.user_growth import plot_user_growth
+from configurations.config import days
+from database.db import get_top_users, get_top_mentioners, get_active_channels, get_total_messages, \
+    get_total_links_shared, get_message_with_mentions_count, get_most_mentioned_users, get_busiest_hour, \
+    get_busiest_day, get_unique_users, get_avg_messages_per_user, get_user_growth_over_time
 
 
 async def handle_serverstats(message):
-    server = message.guild
-    top_users = fetch_top_users(server.name)
+    server_name = message.guild.name
 
-    stats_message = "**Server Statistics**\n"
-    stats_message += "\n**Top 3 Active Users**:\n"
-    for i, user in enumerate(top_users, 1):
-        stats_message += f"{i}. {user['username']} - {user['message_count']} messages\n"
+    top_users_data = get_top_users(server_name)
+    top_mentions_data = get_top_mentioners(server_name)
+    active_channels_data = get_active_channels(server_name)
+    total_messages = get_total_messages(server_name)
+    total_links = get_total_links_shared(server_name)
+    total_mentions = get_message_with_mentions_count(server_name)
+    most_mentioned_users = get_most_mentioned_users(server_name)
+    busiest_hour = get_busiest_hour(server_name)
+    busiest_day = get_busiest_day(server_name)
+    unique_users = get_unique_users(server_name)
+    avg_messages_per_user = get_avg_messages_per_user(server_name)
+    user_growth_data = get_user_growth_over_time(server_name)
+
+    # Getting the saved image path
+    image_path = plot_user_growth(user_growth_data)
+
+    # Format the fetched data into a message
+    stats_message = f":bar_chart: **Server Statistics for {server_name}**\n"
+
+    stats_message += f"\n:envelope: **Total Messages**: {total_messages}"
+    stats_message += f"\n:link: **Total Links Shared**: {total_links}"
+    stats_message += f"\n:loudspeaker: **Messages with Mentions**: {total_mentions}"
+    stats_message += f"\n⏰ **Busiest Time of Day**: {busiest_hour[1]}:00 - {busiest_hour[1] + 1}:00 on {busiest_hour[0]}"
+    stats_message += f"\n📅 **Busiest Day of the Week**: {days[busiest_day[0]]}"
+    stats_message += f"\n👥 **Number of Unique Users**: {unique_users}"
+    stats_message += f"\n📩 **Average Messages per User**: {avg_messages_per_user:.2f}"
+
+    stats_message += "\n\n:trophy: **Top 3 Active Users**:\n"
+    for i, user in enumerate(top_users_data, 1):
+        stats_message += f"{i}. :bust_in_silhouette: {user[0]} - {user[1]} messages\n"
+
+    stats_message += "\n\n:star2: **Top 3 Mentioners**:\n"
+    for i, mention in enumerate(top_mentions_data, 1):
+        stats_message += f"{i}. :busts_in_silhouette: {mention[0]} - {mention[1]} times\n"
+
+    stats_message += "\n\n:loud_sound: **Top 3 Active Channels**:\n"
+    for i, channel in enumerate(active_channels_data, 1):
+        stats_message += f"{i}. :speech_balloon: {channel[0]} - {channel[1]} messages\n"
+
+    # stats_message += "\n\n🏆 **Top 3 Most Mentioned Users**:\n"
+    # for i, user in enumerate(most_mentioned_users, 1):
+    #     stats_message += f"{i}. 📣 {user[0]} - {user[1]} mentions\n"
 
     await message.channel.send(stats_message)
+
+    # Sending the image in Discord
+    image = File(image_path, filename="user_growth.png")
+    await message.channel.send("📈 **User Growth Over Time**", file=image)
--- database/db.py
+++ database/db.py
@@ -1,5 +1,6 @@
 import asyncio
 import pyodbc
+from collections import Counter
 from configurations import keys
 from database.message_extractors import extract_mentions, extract_links
 
@@ -47,3 +48,189 @@ def store_message_in_db(username, servername, channel, message, timestamp, menti
     finally:
         cursor.close()
         cnxn.close()
+
+
+def get_total_messages(servername):
+    cnxn, cursor = connect_to_db()
+    cursor.execute("SELECT COUNT(*) FROM dbo.discord_logs WHERE servername = ?", (servername,))
+    total = cursor.fetchone()[0]
+    cursor.close()
+    cnxn.close()
+    return total
+
+
+def get_top_users(servername, limit=3):
+    cnxn, cursor = connect_to_db()
+    cursor.execute(
+        "SELECT TOP (?) username, COUNT(*) as count FROM dbo.discord_logs WHERE servername = ? GROUP BY username "
+        "ORDER BY count DESC",
+        (limit, servername)
+    )
+    results = cursor.fetchall()
+    cursor.close()
+    cnxn.close()
+    return results
+
+
+def get_total_links_shared(servername):
+    cnxn, cursor = connect_to_db()
+    cursor.execute("SELECT COUNT(*) FROM dbo.discord_logs WHERE servername = ? AND links IS NOT NULL AND links <> ''",
+                   (servername,))
+    total = cursor.fetchone()[0]
+    cursor.close()
+    cnxn.close()
+    return total
+
+
+def get_top_mentioners(servername, limit=3):
+    cnxn, cursor = connect_to_db()
+    cursor.execute(
+        "SELECT TOP (?) username, COUNT(mentions) as count FROM dbo.discord_logs WHERE servername = ? AND mentions IS "
+        "NOT NULL AND mentions <> '' GROUP BY username ORDER BY count DESC",
+        (limit, servername)
+    )
+    results = cursor.fetchall()
+    cursor.close()
+    cnxn.close()
+    return results
+
+
+def get_active_channels(servername, limit=3):
+    cnxn, cursor = connect_to_db()
+    cursor.execute(
+        "SELECT TOP (?) channel, COUNT(*) as count FROM dbo.discord_logs WHERE servername = ? GROUP BY channel ORDER "
+        "BY count DESC",
+        (limit, servername)
+    )
+    results = cursor.fetchall()
+    cursor.close()
+    cnxn.close()
+    return results
+
+
+def get_messages_over_time(servername, interval="daily"):
+    cnxn, cursor = connect_to_db()
+    if interval == "daily":
+        cursor.execute("SELECT FORMAT(timestamp, 'yyyy-MM-dd') as date, COUNT(*) as count FROM dbo.discord_logs WHERE "
+                       "servername = ? GROUP BY FORMAT(timestamp, 'yyyy-MM-dd') ORDER BY date DESC", (servername,))
+    elif interval == "weekly":
+        cursor.execute("SELECT FORMAT(timestamp, 'yyyy-MM-dd', 'en-US') as date, COUNT(*) as count FROM "
+                       "dbo.discord_logs WHERE servername = ? GROUP BY DATEPART(week, timestamp) ORDER BY date DESC",
+                       (servername,))
+    elif interval == "monthly":
+        cursor.execute("SELECT FORMAT(timestamp, 'yyyy-MM') as date, COUNT(*) as count FROM dbo.discord_logs WHERE "
+                       "servername = ? GROUP BY FORMAT(timestamp, 'yyyy-MM') ORDER BY date DESC", (servername,))
+    results = cursor.fetchall()
+    cursor.close()
+    cnxn.close()
+    return results
+
+
+def get_message_with_mentions_count(servername):
+    cnxn, cursor = connect_to_db()
+    cursor.execute("SELECT COUNT(*) FROM dbo.discord_logs WHERE servername = ? AND mentions IS NOT NULL AND mentions "
+                   "<> ''", (servername,))
+    total = cursor.fetchone()[0]
+    cursor.close()
+    cnxn.close()
+    return total
+
+
+def get_most_mentioned_users(servername, limit=3):
+    cnxn, cursor = connect_to_db()
+    cursor.execute(
+        """
+        SELECT mentions
+        FROM dbo.discord_logs 
+        WHERE servername = ? AND mentions IS NOT NULL AND mentions <> ''
+        """,
+        (servername,)
+    )
+
+    rows = cursor.fetchall()
+    cursor.close()
+    cnxn.close()
+
+    mentions = []
+    for row in rows:
+        mentions_in_row = row[0].split(',')
+        mentions.extend(mentions_in_row)
+
+    mention_counts = Counter(mentions)
+    return mention_counts.most_common(limit)
+
+
+def get_busiest_hour(servername):
+    cnxn, cursor = connect_to_db()
+    cursor.execute(
+        """
+        SELECT DATEPART(HOUR, timestamp) as hour, COUNT(*) as count 
+        FROM dbo.discord_logs 
+        WHERE servername = ? 
+        GROUP BY DATEPART(HOUR, timestamp) 
+        ORDER BY count DESC 
+        """,
+        (servername,)
+    )
+    results = cursor.fetchone()
+    cursor.close()
+    cnxn.close()
+    return results
+
+
+def get_busiest_day(servername):
+    cnxn, cursor = connect_to_db()
+    cursor.execute(
+        """
+        SELECT DATEPART(WEEKDAY, timestamp) as day, COUNT(*) as count 
+        FROM dbo.discord_logs 
+        WHERE servername = ? 
+        GROUP BY DATEPART(WEEKDAY, timestamp) 
+        ORDER BY count DESC 
+        """,
+        (servername,)
+    )
+    results = cursor.fetchone()
+    cursor.close()
+    cnxn.close()
+    return results
+
+
+def get_unique_users(servername):
+    cnxn, cursor = connect_to_db()
+    cursor.execute(
+        """
+        SELECT COUNT(DISTINCT username) 
+        FROM dbo.discord_logs 
+        WHERE servername = ?
+        """,
+        (servername,)
+    )
+    total = cursor.fetchone()[0]
+    cursor.close()
+    cnxn.close()
+    return total
+
+
+def get_avg_messages_per_user(servername):
+    total_messages = get_total_messages(servername)
+    unique_users = get_unique_users(servername)
+    return total_messages / unique_users if unique_users != 0 else 0
+
+
+def get_user_growth_over_time(servername):
+    cnxn, cursor = connect_to_db()
+    cursor.execute(
+        """
+        SELECT FORMAT(timestamp, 'yyyy-MM-dd') as date, COUNT(DISTINCT username) as new_users
+        FROM dbo.discord_logs 
+        WHERE servername = ? 
+        GROUP BY FORMAT(timestamp, 'yyyy-MM-dd') 
+        ORDER BY date ASC
+        """,
+        (servername,)
+    )
+    results = cursor.fetchall()
+    cursor.close()
+    cnxn.close()
+    return results
