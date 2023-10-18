f680c9358d48ee5daae361d6b62776c3e7c84c94
Byron <80455013+jyronbones@users.noreply.github.com>
2023-10-18 00:30:53 -0400

    refactore files

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
 import asyncio
 from configurations.config import client
-from discord_bot import events
+from controller import event_controller
 from utilities import keys
 
 
 @client.event
 async def on_ready():
-    await events.on_ready(client)
+    await event_controller.on_ready(client)
 
 
 @client.event
 async def on_message(message):
-    await events.on_message(client, message)
+    await event_controller.on_message(client, message)
 
 if __name__ == "__main__":
     try:
